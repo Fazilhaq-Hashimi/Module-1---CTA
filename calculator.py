8d27f1a2253b4b2ca63484819171f42fd3afba01
Fazilhaq Hashimi <fazilhaq.hashimi@csuglobal.edu>
2026-02-16 23:50:10 -0700

    Add basic arithmetic operations to calculator
    Implement basic arithmetic operations: addition, subtraction, multiplication, and division.

---
+++ calculator.py
@@ -0,0 +1,30 @@
+# Module 1: Critical Thinking Assignment at Colorado State University Global 
+# Program: Basic Arithmetic Operations 
+# Purpose: Show addition, subtraction, multiplication, and dvision 
+# ---------------------------------------------------------------------------
+
+# Part 1: Adding and Subtracting 
+
+print("Welcome to the Module 1's Critical Thinking Assignment for the Python Calculator Program") 
+
+# Asking the user for input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculating results 
+addition = num1 + num2
+subtraction = num1 = num2 
+
+# Displaying Part 1's Output
+print("The addition of these numbers is:", addition) 
+print("The subtraction of these numbers is:", subtraction) 
+
+# Part 2: Multiplication and Division
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying Part 2's Output
+print("The multiplication of these numbers is:", multiplication) 
+print("The division of these numbers is:", division)
+
+
